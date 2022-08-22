ac493325dbc20603a7297565b93ef03d9ae43c99
vih0 <vitoria.s.araujo1020@gmail.com>
2022-08-22 14:48:03 -0300

    adicionando Icones menu lateral

--- src/components/menuLateral.js
+++ src/components/menuLateral.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import {RiHome2Fill,RiLogoutBoxRLine} from 'react-icons/ri';
+import {BsFillBookmarkFill,BsFillPersonFill,BsFillGearFill,BsFillInfoCircleFill,} from 'react-icons/bs';
+
+
 
 function MenuLateral(){
     return(
         <div>
             <div>
-                
+                <nav>
+                    <li><RiHome2Fill/>Home</li>
+                    <li><BsFillBookmarkFill/>Cursos</li>
+                    <li><BsFillPersonFill/>Perfil</li>
+                    <li><BsFillGearFill/>Configurações</li>
+                    <li><BsFillInfoCircleFill/>Sobre</li>
+                    <li><RiLogoutBoxRLine/>Sair</li>
+
+                </nav>
             </div>
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,13 +4,14 @@ import ReactDOM from 'react-dom/client';
 import Welcome from './pages/welcome'
 import Home from './pages/home'
 import './index.css'
-
+import MenuLateral from './components/menuLateral';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     
     {/* <Home/> */}
-    <Welcome />
+    {/* <Welcome /> */}
+    <MenuLateral/>
   </React.StrictMode>
 );
 
