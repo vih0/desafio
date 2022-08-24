826261b639eef0bc9df6fe1f8a92f76e7d381cdf
vih0 <vitoria.s.araujo1020@gmail.com>
2022-08-24 10:43:59 -0300

    Barra de pesquisa finish

--- src/components/barraPesquisa.js
+++ src/components/barraPesquisa.js
@@ -2,17 +2,20 @@ import React from 'react';
 import {FiSearch} from 'react-icons/fi';
 import{BsGear} from 'react-icons/bs'
 import{IoMdNotificationsOutline} from 'react-icons/io'
+import './barraPesquisa.css';
 
 
 function BarraPesquisa(){
     return(
         <div class ="barraPesquisa">
             <div class="Icones">
-             <IoMdNotificationsOutline size ="20"/>   
-            <BsGear size ="20"/>
+             <IoMdNotificationsOutline size ="20" color =" #808080"/>   
+            <BsGear size ="20" color = "#808080"/>
             </div>
+            <div class = "search">
             <FiSearch size ="15" color=" #8D8D8D"/>
-            <input type ="text" placeholder ="Pesquisa"></input>
+            <input type ="text" placeholder ="Pesquisa" ></input>
+            </div>
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
-import Welcome from './pages/welcome'
-import Home from './pages/home'
 import './pages/home.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
